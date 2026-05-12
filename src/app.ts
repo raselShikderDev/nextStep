ede0709b80bd43f165cbfa546a5709f4a6cceef1
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2026-05-12 21:48:25 +0600

    iniliazied schemas

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@
 import express, { type Application, type Request, type Response } from 'express';
 import cookieParser from 'cookie-parser'
 import helmet from 'helmet'
+import morgan from 'morgan'
 
 const expressApp: Application = express()
 
 expressApp.use(helmet())
 expressApp.use(express.json())
 expressApp.use(cookieParser())
+expressApp.use(morgan('dev'));
 
 expressApp.get("/",(_req:Request, res:Response)=>{
     res.send("App running")
--- src/errorHelper/appError.ts
+++ src/errorHelper/appError.ts
@@ -12,3 +12,5 @@ class appError extends Error{
         }
     }
 }
+
+export default appError
