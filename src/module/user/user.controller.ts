3de12df081183d4c5df8db8768861017a467c119
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2026-05-17 23:56:49 +0600

    added services route

--- src/module/user/user.controller.ts
+++ src/module/user/user.controller.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const router = Router()
 
-router.post("/login", ()=>{console.log("Login router hit");
+router.post("/create-user", ()=>{console.log("User router hit");
 })
 
 
