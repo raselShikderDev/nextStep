98c05a0fb6adcfacf06d638936360188d8a38e6e
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2026-05-20 01:14:24 +0600

    refactored zod validator

--- src/middleware/requestZodValidator.ts
+++ src/middleware/requestZodValidator.ts
@@ -1,38 +1,37 @@
-import envVar from "@/config/env.config";
 import type { NextFunction, Request, Response } from "express";
-import { parseAsync, type ZodObject, type ZodRawShape } from "zod";
+import envVar from "@/config/env.config";
+import type { ZodObject, ZodRawShape } from "zod";
 
 const requestZodValidator =
-  (schmea: ZodObject<ZodRawShape>) =>
-  async (req: Request, res: Response, next: NextFunction) => {
-   try {
+	(schema: ZodObject<ZodRawShape>) =>
+	async (
+		req: Request,
+		res: Response,
+		next: NextFunction,
+	) => {
+		try {
+			if (envVar.NODE_ENV === "Development") {
+				console.log(
+					"[Zod Validator] Incoming Request:",
+					req.body,
+				);
+			}
 
-     if (envVar.NODE_ENV === 'Development') {
-        console.log(
-          '[requestZodValidator] Befire Zod validation - payload:',
-          req.body,
-        );
-      }
+			req.body = await schema.parseAsync(
+				req.body,
+			);
 
+			if (envVar.NODE_ENV === "Development") {
+				console.log(
+					"[Zod Validator] Validated Request:",
+					req.body,
+				);
+			}
 
-     if (req.body) {
-      req.body = JSON.parse(req.body);
-    } else if (req.body.data) {
-      req.body = JSON.parse(req.body.data);
-    }
-
-   
-      req.body = await schmea.parseAsync(req.body)
-       if (envVar.NODE_ENV === 'Development') {
-        console.log(
-          '[requestValidator] After Zod validation - payload:',
-          req.body,
-        );
-      }
-      next()
-   } catch (error) {
-    next(error)
-   }
-  };
+			next();
+		} catch (error) {
+			next(error);
+		}
+	};
 
 export default requestZodValidator;
--- src/module/auth/auth.route.ts
+++ src/module/auth/auth.route.ts
@@ -3,8 +3,28 @@ import { AuthControllers } from "./auth.controller";
 
 const router = Router();
 
+// Register user
 router.post("/register", AuthControllers.registerUser);
 
+// Login User
 router.post("/login", AuthControllers.loginUser);
+
+// Send otp for reseting password after forgetting
+router.post(
+	"/forgot-password",
+	AuthControllers.forgotPassword,
+);
+
+// Reset password after forgeting
+router.post(
+	"/reset-password",
+	AuthControllers.resetPassword,
+);
+
+// Chnage password - For logged in user
+router.post(
+	"/change-password",
+	AuthControllers.changePassword,
+);
 
 export const authRouter = router;
