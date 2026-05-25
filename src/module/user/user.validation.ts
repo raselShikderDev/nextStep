b9f12dbdf8b0c03d33a1c48b5f0bbc33fe4b029c
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2026-05-25 23:48:51 +0600

    chnaged zod for update user validation

--- src/module/user/user.validation.ts
+++ src/module/user/user.validation.ts
@@ -1,16 +1,14 @@
 import { z } from "zod";
 
-// biome-ignore lint/suspicious/noUselessEscapeInString: >
-const phone = "/^\+\d{1,15}$/";
 
 export const updateUserValidationSchema = z.object({
 	name: z.string().min(2, "Name must be at least 2 characters").optional(),
 	phone: z
 		.string()
-		.regex(new RegExp(phone), "Invalid phone number format")
+		.regex(/^(?:\+8801|01)[3-9]\d{8}$/, "Invalid phone number format")
 		.optional(),
 	address: z.string().optional(),
-	avatarUrl: z.url().optional(),
+	avatarUrl: z.string().url().optional(),
 });
 
 export const requestEmailChangeSchema = z.object({
