722f7bc7133cba98405f695f9aa9e359ab04f250
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2026-05-30 23:50:18 +0600

    fixed database isuues

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,15 +1,5 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-
 import { PrismaClient } from "../../prisma/generated/prisma/client";
 
-import envVar from "./env.config";
-
-const adapter = new PrismaPg({
-	connectionString: envVar.DATABASE_URL,
-});
-
-const prisma = new PrismaClient({
-	adapter,
-});
+const prisma = new PrismaClient({});
 
 export default prisma;
